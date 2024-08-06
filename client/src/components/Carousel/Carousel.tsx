21ec7a30b38237148fccb1f1742488dc12913cca
TarasOstasha <tonyjoss1990@gmail.com>
2024-08-05 20:45:25 -0400

    add carousel static imgs

--- client/src/components/Carousel/Carousel.tsx
+++ client/src/components/Carousel/Carousel.tsx
@@ -4,7 +4,34 @@ import { Box, Typography } from '@mui/material';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
+
+interface CarouselData {
+  img: string;
+  title: string;
+  description: string;
+}
+
+const carouselData: CarouselData[] = [
+  {
+    img: 'https://www.kauai.com/images/haena-beach-bali-hai-scaled.jpg',
+    title: 'Kaui',
+    description: 'dfdsfd hdjshf dshf jdkshf sdhfhsdf'
+  },
+  {
+    img: 'https://www.kauai.com/images/haena-beach-bali-hai-scaled.jpg',
+    title: 'Kaui',
+    description: 'dfdsfd hdjshf dshf jdkshf sdhfhsdf'
+  },
+  {
+    img: 'https://www.kauai.com/images/haena-beach-bali-hai-scaled.jpg',
+    title: 'Kaui',
+    description: 'dfdsfd hdjshf dshf jdkshf sdhfhsdf'
+  }
+]
+
 const Carousel: React.FC = () => {
+
+  
   const settings = {
     dots: true,
     infinite: true,
@@ -13,21 +40,27 @@ const Carousel: React.FC = () => {
     slidesToScroll: 1
   };
 
+  // https://www.kauai.com/images/haena-beach-bali-hai-scaled.jpg
+
+
+
+
   return (
-    <Box sx={{ maxWidth: 600, margin: '0 auto', paddingTop: 4 }}>
+    <Box sx={{ maxWidth: 1200, margin: '0 auto', paddingTop: 4 }}>
       <Slider {...settings}>
-        <Box>
-          <Typography variant="h4">Slide 1</Typography>
-          <Typography>Content for Slide 1</Typography>
-        </Box>
-        <Box>
-          <Typography variant="h4">Slide 2</Typography>
-          <Typography>Content for Slide 2</Typography>
-        </Box>
-        <Box>
-          <Typography variant="h4">Slide 3</Typography>
-          <Typography>Content for Slide 3</Typography>
-        </Box>
+        {carouselData.map((item, index) => (
+          <Box key={index} sx={{ textAlign: 'center' }}>
+            <img
+              src={item.img}
+              alt={item.title}
+              style={{ width: '100%', height: 'auto', marginBottom: 16 }}
+            />
+            <Typography variant="h4">{item.title}</Typography>
+            <Typography variant="body1" component="p">
+              {item.description}
+            </Typography>
+          </Box>
+        ))}
       </Slider>
     </Box>
   );
