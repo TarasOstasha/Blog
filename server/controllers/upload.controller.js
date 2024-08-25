a228d0c3b92a8f87e1cb8eebfae77ac045b882f3
TarasOstasha <tonyjoss1990@gmail.com>
2024-08-24 20:19:52 -0400

    add getimgs gallery

--- server/controllers/upload.controller.js
+++ server/controllers/upload.controller.js
@@ -3,7 +3,7 @@ const { Gallery } = require('./../models');
 exports.uploadImg = async (req, res, next) => {
   const { title, author } = req.body; // Text fields sent with the form
   const { filename: fileName } = req.file; // The uploaded file
-
+  console.log(req.file);
   if (!fileName) {
     return res.status(400).json({ message: 'Image upload failed' });
   }
@@ -12,7 +12,7 @@ exports.uploadImg = async (req, res, next) => {
     const newImage = await Gallery.create({
       title,
       author,
-      fileName,
+      fileName: `/images/gallery/${fileName}`,
     });
 
     res.status(200).json({
@@ -43,9 +43,12 @@ exports.uploadImg = async (req, res, next) => {
 
 exports.getImgs = async (req, res, next) => {
   try {
+    const { limit = 10, offset = 0 } = req.query;
     const foundImgs = await Gallery.findAll({
       attributes: { exclude: ['createdAt', 'updatedAt'] }, // Exclude these fields
       raw: true, // Return raw data instead of Sequelize instances
+      limit: parseInt(limit, 10), // Ensure limit is an integer
+      offset: parseInt(offset, 10), // Ensure offset is an integer
     });
     res.status(200).json({
       data: foundImgs,
